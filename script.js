c62cb0314efe33a33b6f16634116043ed91a9ebc
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-02-20 11:00:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+var butt=document.getElementById("btn")
+butt.addEventListener('click',guessNum)
+var val=document.getElementById("num")
+var guess=document.getElementById("guess")
+var responce=document.getElementById("respond")
+function guessNum() {
+	var random=Math.floor(Math.random()*40)-20
+	val.innerText=random
+	if (Math.abs(Number(guess.value)-Number(random))<=5) {
+		responce.innerText="Hot"
+	}
+	else responce.innerText="Cold"
+}
